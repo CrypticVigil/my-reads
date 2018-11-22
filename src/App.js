2fa889ab25461419a1b270e3d8170d66b2bd2621
chadbarrows <chad.m.barrows@gmail.com>
2018-11-22 09:57:43 -0500

    added routing and separated app and search pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react'
+import { Route, Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import Book from './Book'
+import Search from './Search'
 import './App.css'
 
 class BooksApp extends React.Component {
 	state = {
-		/**
-		 * TODO: Instead of using this state variable to keep track of which page
-		 * we're on, use the URL in the browser's address bar. This will ensure that
-		 * users can use the browser's back and forward buttons to navigate between
-		 * pages, as well as provide a good URL they can bookmark and share.
-		 */
-		showSearchPage: false,
-
 		books: []
 	}
 
@@ -24,74 +18,59 @@ class BooksApp extends React.Component {
 	render() {
 		return (
 			<div className="app">
-				{this.state.showSearchPage ? (
-					<div className="search-books">
-						<div className="search-books-bar">
-							<button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-							<div className="search-books-input-wrapper">
-								{/*
-									NOTES: The search from BooksAPI is limited to a particular set of search terms.
-									You can find these search terms here:
-									https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-									However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-									you don't find a specific author or title. Every search is limited by search terms.
-								*/}
-								<input type="text" placeholder="Search by title or author"/>
-
-							</div>
-						</div>
-						<div className="search-books-results">
-							<ol className="books-grid"></ol>
-						</div>
+			<Route exact path='/' render={() => (
+				<div className="list-books">
+					<div className="list-books-title">
+						<h1>My<span className='title-right'>Reads</span></h1>
 					</div>
-				) : (
-					<div className="list-books">
-						<div className="list-books-title">
-							<h1>My<span className='title-right'>Reads</span></h1>
-						</div>
-						<div className="list-books-content">
-							<div>
-								<div className="bookshelf">
-									<h2 className="bookshelf-title">Currently Reading</h2>
-									<div className="bookshelf-books">
-										<ol className="books-grid">
-											{this.state.books.filter( (book) => book.shelf === 'currentlyReading')
+					<div className="list-books-content">
+						<div>
+							<div className="bookshelf">
+								<h2 className="bookshelf-title">Currently Reading</h2>
+								<div className="bookshelf-books">
+									<ol className="books-grid">
+										{this.state.books.filter( (book) => book.shelf === 'currentlyReading')
+										.map( (book) => 
+											<Book current={book} key={book.id} /> 
+										)}
+									</ol>
+								</div>
+							</div>
+							<div className="bookshelf">
+								<h2 className="bookshelf-title">Want to Read</h2>
+								<div className="bookshelf-books">
+									<ol className="books-grid">
+									{this.state.books.filter( (book) => book.shelf === 'wantToRead')
+										.map( (book) => 
+											<Book current={book} key={book.id} /> 
+										)}
+									</ol>
+								</div>
+							</div>
+							<div className="bookshelf">
+								<h2 className="bookshelf-title">Read</h2>
+								<div className="bookshelf-books">
+									<ol className="books-grid">
+										{this.state.books.filter( (book) => book.shelf === 'read')
 											.map( (book) => 
 												<Book current={book} key={book.id} /> 
-											)}
-										</ol>
-									</div>
-								</div>
-								<div className="bookshelf">
-									<h2 className="bookshelf-title">Want to Read</h2>
-									<div className="bookshelf-books">
-										<ol className="books-grid">
-										{this.state.books.filter( (book) => book.shelf === 'wantToRead')
-											.map( (book) => 
-												<Book current={book} key={book.id} /> 
-											)}
-										</ol>
-									</div>
-								</div>
-								<div className="bookshelf">
-									<h2 className="bookshelf-title">Read</h2>
-									<div className="bookshelf-books">
-										<ol className="books-grid">
-											{this.state.books.filter( (book) => book.shelf === 'read')
-												.map( (book) => 
-													<Book current={book} key={book.id} /> 
-											)}
-										</ol>
-									</div>
+										)}
+									</ol>
 								</div>
 							</div>
 						</div>
-						<div className="open-search">
-							<button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-						</div>
 					</div>
-				)}
+					<div className="open-search">
+						<Link to='/search'>Find a Book</Link>
+					</div>
+				</div>
+				
+			)} />
+
+			<Route path='/search' render={ () => (
+				<Search />	
+			)} />
 			</div>
 		)
 	}
